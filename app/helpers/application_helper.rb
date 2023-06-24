13e79f5880b026eefdd80bddb2291f4308b64ac0
Jose Lucas <lucasnba2324@gmail.com>
2023-06-23 22:47:30 -0300

    definindo metodo no helper para o status das tasks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def task_status_class(task)
+    if task.completed
+      return "border border-1 border-success"
+    elsif task.delivery_date < Date.today
+      return "border border-1 border-danger"
+    elsif task.delivery_date > Date.today + 1
+      return "border border-1 border-primary"
+    else
+      return "border border-1 border-warning"
+    end
+  end
 end
