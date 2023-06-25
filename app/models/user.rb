72007f9c9f166c5d1870d89961f91aa1665d2bfd
Jose Lucas <lucasnba2324@gmail.com>
2023-06-25 19:14:59 -0300

    criando metodo de instancia para pegar o nome completo do usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          has_many :tasks, dependent: :destroy
+
+  def full_name
+    [self.first_name, self.last_name].join(' ')
+  end
 end
