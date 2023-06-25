da42ed9873b716cad72032f511948110a7011f96
Jose Lucas <lucasnba2324@gmail.com>
2023-06-25 19:49:53 -0300

    Configurando metodos para atualizacao dos dados do usuario

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,32 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :verify_password, only: [:update]
+  before_action :set_user
+
   def edit
-    @user = User.find(current_user.id)
   end
 
   def update
-    
+    if @user.update(params_user)
+      redirect_to edit_profiles_path, notice: 'Profile updated successfully', toastr: "success"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def params_user
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def verify_password
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+      params[:user].extract!(:password, :password_confirmation)
+    end
   end
 end
