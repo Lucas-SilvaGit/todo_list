a9894e7ab2a9ba454a097fd26313943f4e0370ce
Jose Lucas <lucasnba2324@gmail.com>
2023-06-25 23:31:56 -0300

    informando que user tem um attached avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          has_many :tasks, dependent: :destroy
+         has_one_attached :avatar
 
   def full_name
     [self.first_name, self.last_name].join(' ')
