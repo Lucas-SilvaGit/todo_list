c8f4fcf692f1f54f1a880320cf4340d06baab617
Jose Lucas <lucasnba2324@gmail.com>
2023-06-26 21:12:56 -0300

    criando controllers do devise e configurando os controllers de session e registration

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,22 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+
+    if resource.persisted?
+      set_flash_message! :notice, :signed_up
+      sign_up(resource_name, resource)
+      respond_with resource, location: after_sign_up_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      resource.errors.full_messages.each do |message|
+        flash[:error] = message
+        toastr[:error] = message
+      end
+      redirect_to new_user_registration_path
+    end
+  end
+end
