1a3196501473947ad6a7a6b8c1a0cc112d266ac3
Jose Lucas <lucasnba2324@gmail.com>
2023-06-29 23:48:51 -0300

    cria as permissoes de forma explicita para permitir que as associacoes de Task sejam pesquisaveis pelo ransack

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,12 @@ class Task < ApplicationRecord
   belongs_to :user
 
   validates :title, :body, :delivery_date, presence: true
+  
+  def self.ransackable_attributes(auth_object = nil)
+    ["body", "completed", "created_at", "delivery_date", "id", "title", "updated_at", "user_id"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["user"]
+  end
 end
