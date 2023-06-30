a3a83396932afee0051fa2f0e9e4959e4d81d792
Jose Lucas <lucasnba2324@gmail.com>
2023-06-29 23:49:51 -0300

    configurando as rotas para o controller search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :tasks
-  devise_for :users, controllers: { sessions: 'users/sessions' }
   resource :profiles, only: [:edit, :update]
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  
+  get 'search', to: 'search#index'
 
   root 'tasks#index'
 end
