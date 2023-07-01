6e2c5322cf57d626f8cdb86ecabcd6781f74b3a5
Jose Lucas <lucasnba2324@gmail.com>
2023-06-30 22:50:57 -0300

    removendo o controller search por completo

--- app/controllers/search_controller.rb
+++
@@ -1,8 +0,0 @@
-class SearchController < ApplicationController
-  before_action :authenticate_user!
-  
-  def index
-    @query = Task.ransack(params[:q])
-    @tasks = @query.result(distinct: true)
-  end
-end
