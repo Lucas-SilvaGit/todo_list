232319e1e61439490b9b062faaabc5db617114f8
Jose Lucas <lucasnba2324@gmail.com>
2023-06-30 22:50:37 -0300

    removendo rotas do controller search

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :tasks
   resource :profiles, only: [:edit, :update]
   devise_for :users, controllers: { sessions: 'users/sessions' }
-  
-  get 'search', to: 'search#index'
 
   root 'tasks#index'
 end
