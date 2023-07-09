fdafda88f2657cd06d6132730deb650c2e90f984
Jose Lucas <lucasnba2324@gmail.com>
2023-07-09 11:06:19 -0300

    removendo o metodo send_welcome_email, e a linha toastr error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  
   def create
     build_resource(sign_up_params)
 
@@ -8,21 +9,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
     if resource.persisted?
       set_flash_message! :notice, :signed_up
       sign_up(resource_name, resource)
-      send_welcome_email(resource)
       respond_with resource, location: after_sign_up_path_for(resource)
     else
       clean_up_passwords resource
       set_minimum_password_length
       resource.errors.full_messages.each do |message|
         flash[:error] = message
-        toastr[:error] = message
       end
       redirect_to new_user_registration_path
     end
-  end
-
-  def send_welcome_email(user)
-    UserMailer.welcome_email(user).deliver_now    
-    Rails.logger.info("E-mail de boas-vindas enviado para #{user.email}")
   end
 end
