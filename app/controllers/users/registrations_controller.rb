563c8cc5f7ab0809dd559e7d6716b6ed8df4faf8
Jose Lucas <lucasnba2324@gmail.com>
2023-07-09 18:37:17 -0300

    corrigindo o metodo create do registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,21 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  
   def create
     build_resource(sign_up_params)
 
-    resource.save
-    yield resource if block_given?
-
-    if resource.persisted?
+    if resource.save
       set_flash_message! :notice, :signed_up
       sign_up(resource_name, resource)
       respond_with resource, location: after_sign_up_path_for(resource)
     else
       clean_up_passwords resource
       set_minimum_password_length
-      resource.errors.full_messages.each do |message|
-        flash[:error] = message
-      end
+      flash[:error] = resource.errors.full_messages.join(", ")
       redirect_to new_user_registration_path
     end
   end
