02e15eaa91799e0a62da0b49b3191b838c1b7696
Jose Lucas <lucasnba2324@gmail.com>
2023-07-09 08:47:07 -0300

    adicionando registrations nas rotas do controller do devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
   resource :profiles, only: [:edit, :update]
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
   root 'tasks#index'
 end
