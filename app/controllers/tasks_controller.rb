75f0941313b64dba09f1192a03bfa15eebc13ce5
Jose Lucas <lucasnba2324@gmail.com>
2023-07-11 23:03:45 -0300

    permitindo o novo campo no controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -77,6 +77,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :body, :delivery_date, :completed)
+      params.require(:task).permit(:title, :body, :delivery_date, :completed, :reminder)
     end
 end
