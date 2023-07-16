9f4546ff403f5afd187768f1794454975010df94
Jose Lucas <lucasnba2324@gmail.com>
2023-07-16 01:42:45 -0300

    adicionada rota para autenticacao via omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :tasks
   resource :profiles, only: [:edit, :update]
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  } 
 
   root 'tasks#index'
 end
