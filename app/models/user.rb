f599c97ebb7ec1c9b745c9ab8d73be1fea0f52df
Jose Lucas <lucasnba2324@gmail.com>
2023-07-16 01:41:41 -0300

    criando o metodo para autenticacao via omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable, :omniauthable
          has_many :tasks, dependent: :destroy
          has_one_attached :avatar
 
   def full_name
     [self.first_name, self.last_name].join(' ')
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+    end
   end
 end
