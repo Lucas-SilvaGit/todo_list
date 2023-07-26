797aeed5802d4feeebad7d84be69f276e36114a9
Jose Lucas <lucasnba2324@gmail.com>
2023-07-26 00:06:37 -0300

    validando o body da task

--- spec/models/tasks_spec.rb
+++ spec/models/tasks_spec.rb
@@ -3,31 +3,49 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
 
   describe "Task Creation" do
-    it 'creates a new task' do
-      user = create(:user)
-      task = create(:task, user: user)
+    context "when data is valid" do
+      it 'creates a new task' do
+        user = create(:user)
+        task = create(:task, user: user)
 
-      expect(task.title).to eq('Task Title')
-      expect(task.body).to  eq('Task Body')
-      expect(task.delivery_date).to eq(Date.today)
-      expect([true,false]).to include(task.completed)  
-      expect([true, false]).to include(task.reminder) 
-      expect(task.user).to eq(user) 
-      expect(task).to be_valid  
+        expect(task.title).to eq('Task Title')
+        expect(task.body).to  eq('Task Body')
+        expect(task.delivery_date).to eq(Date.today)
+        expect([true,false]).to include(task.completed)  
+        expect([true, false]).to include(task.reminder) 
+        expect(task.user).to eq(user) 
+        expect(task).to be_valid  
+      end
     end
+  
+    context "when data is in-valid" do
+      it 'validates presence of title' do
+        user = create(:user)
+        task = build(:task, user: user, title: nil)
+        expect(task.valid?).to be_falsey
+        expect(task.errors[:title]).to include("can't be blank")  
+      end
 
-    it 'validates presence of title' do
-      user = create(:user)
-      task = build(:task, user: user, title: nil)
-      expect(task.valid?).to be_falsey
-      expect(task.errors[:title]).to include("can't be blank")  
-    end
+      it 'validates maximum length of title' do
+        user = create(:user)
+        task = build(:task, user: user, title: "a" * 51)
+        expect(task.valid?).to be_falsey
+        expect(task.errors[:title]).to include("is too long (maximum is 50 characters)")   
+      end
 
-    it 'validates maximum length of title' do
-      user = create(:user)
-      task = build(:task, user: user, title: "a" * 51)
-      expect(task.valid?).to be_falsey
-      expect(task.errors[:title]).to include("is too long (maximum is 50 characters)")   
+      it 'validates presence of body' do
+        user = create(:user)
+        task = build(:task, user: user, body: nil)
+        expect(task.valid?).to be_falsey
+        expect(task.errors[:body]).to include("can't be blank")
+      end
+
+      it 'validates maximum length of body' do
+        user = create(:user)
+        task = build(:task, user: user, body: "a" * 101)
+        expect(task.valid?).to be_falsey  
+        expect(task.errors[:body]).to include("is too long (maximum is 100 characters)")
+      end
     end
   end
 end
